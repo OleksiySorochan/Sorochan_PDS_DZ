7942a17349e3392c205f0da21ca62346d1741151
OleksiySorochan <oleksiy.sorochan@gmail.com>
2023-06-02 22:21:40 +0300

    update dz_10_02.py master

--- Lesson_10/dz_10_02.py
+++ Lesson_10/dz_10_02.py
@@ -1,17 +1,22 @@
 def unicum_namber(lst):
     try:
         a = 0
-        for i in lst:
-            if lst.count(i) > 1:
-                a +=1
+        if lst == []:
+            raise ValueError('Цей список порожній, программа приймає список із чисел.')
+        else:
+            for i in lst:
+                if type(i) is int:
+                    if lst.count(i) > 1:
+                        a +=1
+                else:
+                    raise ValueError('Программа приймає список чисел.')
         if a > 0:
             return f'Числа в цьому списку не є унікальними'
         else:
             return f'Числа в цьому списку  є унікальними'
-    except AttributeError as ex:
+    except ValueError as ex:
         print(f" {ex} Введіть список чисел")
-    except Exception as ex:
-        print(f" Невідома помилка: {ex}. Программа приймає список числел, і провіряє на унікальность")
+
 
 
 
@@ -20,6 +25,7 @@ def unicum_namber(lst):
 
 d = {'one': 1, 'two': 2}
 my_list = '1, 3, 3, 4, 5, 6, 5, 4, 3'
-my_list2 = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+my_list2 = ['1', 2, 3, 4, 5, 6, 7, 8, 2]
+my_list3 = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 print(unicum_namber(my_list2))
 
