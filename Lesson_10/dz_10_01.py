ddfdb2cf9d4b5e8b96f9dc84cd51433c3a4f5e2a
OleksiySorochan <oleksiy.sorochan@gmail.com>
2023-06-04 17:44:15 +0300

    update dz_10_01.py master

--- Lesson_10/dz_10_01.py
+++ Lesson_10/dz_10_01.py
@@ -1,14 +1,16 @@
 def name_of_manth(number):
-    manths = ('Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень')
+    manths = {1:'Січень', 2: 'Лютий', 3: 'Березень', 4: 'Квітень', 5: 'Травень', 6: 'Червень',
+              7: 'Липень', 8: 'Серпень', 9: 'Вересень', 10: 'Жовтень', 11: 'Листопад', 12:'Грудень'}
     try:
-        return manths[number - 1]
-    except IndexError as ex:
-        return f"{ex}: веддіть номер місяця, число від 1 до 12"
-    except TypeError as ex:
+        return manths[number]
+    except KeyError:
         try:
-            return manths[int(number) - 1]
-        except Exception as ex:
+            return manths[int(number)]
+        except ValueError as ex:
             return f"{ex}: веддіть номер місяця, число від 1 до 12"
+    except Exception as ex:
+        return f"{ex}: веддіть номер місяця, число від 1 до 12"
 
 
-print(name_of_manth(5))
+
+print(name_of_manth(6))
