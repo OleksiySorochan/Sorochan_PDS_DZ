75671b2bbfbead7952bf98712222dcad9817a3f7
OleksiySorochan <oleksiy.sorochan@gmail.com>
2023-06-13 12:20:02 +0300

    update dz_12_01.py | 13.06.23 master

--- Lesson_12/dz_12_01.py
+++ Lesson_12/dz_12_01.py
@@ -1,9 +1,6 @@
-from multiprocessing import Pool
-import threading
-import time, os
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import time
 
-
-# функція для порівяння часу і визначення переможця
 def comparison(*args):
     a = list(args)
     sort = sorted(a)
@@ -11,55 +8,42 @@ def comparison(*args):
 
 comparison_dict = {}
 
-# обчислення факторіалу
 def factorial(n):
     f = 1
     for i in range(1, n + 1):
         f *= i
     return f
 
-# звичайний спосіб
-def normal_way():
-    s = time.time()
-    for i in range(1, 500):
-        factorial(i)
-    nor_time = time.time() - s
-    return nor_time
 
-# threading
-def ran(start=0, finish=0):
-    for i in range(start, finish):
-        factorial(i)
+def main():
+    with ThreadPoolExecutor() as tr:
+        start_time = time.time()
+        res_tr = tr.map(factorial, range(1, 500))
+        tr_time = time.time() - start_time
+        comparison_dict[tr_time] = 'ThreadPoolExecutor'
 
-task_1 = threading.Thread(target=ran, kwargs={'finish': 250})
-task_2 = threading.Thread(target=ran, kwargs={'start': 250, 'finish': 500})
+        # print('вивід значення для ThreadPoolExecutor')
+        # for value in res_tr:
+        #     print(value)
 
+    with ProcessPoolExecutor() as pr:
+        start_time_pr = time.time()
+        res_pr = pr.map(factorial, range(1, 500))
+        pr_time = time.time() -  start_time_pr
+        comparison_dict[pr_time] = 'ProcessPoolExecutor'
+
+        # print('вивід значення для ProcessPoolExecutor')
+        # for value in res_pr:
+        #     print(value)
+
+    com_t = comparison(tr_time, pr_time)
+    res = comparison_dict[com_t]
+    print("Обчислення факторіалу числа від 1 до 500 ProcessPoolExecutor і ThreadPoolExecutor способами")
+    print(f"Самий швидчий варіант: {res}, займає часу: {com_t} секунд")
+    print('*'*50)
+    print(f'{comparison_dict[tr_time]} займає часу: {tr_time} секунд')
+    print(f'{comparison_dict[pr_time]} займає часу: {pr_time} секунд')
 
 
 if __name__ == '__main__':
-    # запуск завдань threading способом
-    s = time.time()
-    task_1.start()
-    task_2.start()
-    task_1.join()
-    task_2.join()
-    th_time = time.time()-s
-    comparison_dict[th_time] = 'threading'
-
-    #pool спосіб
-    cpu = os.cpu_count()
-    with Pool(cpu) as p:
-        s2 = time.time()
-        p.map(factorial, range(1, 500))
-        pl_time = time.time() - s2
-        comparison_dict[pl_time] = 'pool'
-
-    # запуск функції для обчислення звисайним способом
-    nor_time = normal_way()
-    comparison_dict[nor_time] = 'normal_way'
-
-    com_t = comparison(th_time, pl_time, nor_time)
-    res = comparison_dict[com_t]
-    print("Обчислення факторіалу числа від 1 до 500 декількома способами")
-    print(f"Самий швидчий варіант: {res}, займає часу: {com_t}")
-
+    main()
